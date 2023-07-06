ddc04d7d9c5cd9579036dd2a03d5b9a503f4917a
bryllemutia <bryllemutia3@gmail.com>
2023-07-06 12:15:29 +0800

    added router pages post and edit

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -31,6 +31,17 @@ export const addNewPost = createAsyncThunk(
    }
 );
 
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
+   const { id } = initialPost;
+
+   try {
+      const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
+      return response.data;
+   } catch(err) {
+      return err.message;
+   }
+}) 
+
 const postsSlice = createSlice({
    name: "posts",
    initialState,
@@ -112,6 +123,17 @@ const postsSlice = createSlice({
 
             console.log(action.payload);
             state.posts.push(action.payload);
+         }).addCase(updatePost.fulfilled, (state, action) => {
+            if (!action.payload?.id) {
+               console.log('Update not completed!');
+               console.log(action.payload);
+               return;
+            }
+
+            const { id } = action.payload;
+            action.payload.date = new Date().toISOString();
+            const posts = state.posts.filter((post) => post.id !== id);
+            state.posts = [...posts, action.payload];
          });
    },
 });
@@ -120,6 +142,7 @@ const postsSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
 
 // export reducers
 export const { postAdded, reactionAdded } = postsSlice.actions;
